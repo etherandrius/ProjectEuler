7b5d1dd68d0de8fb23edff26a24b8290d7ca47ff
etherandrius <etherandrius@gmail.com>
2016-07-26 18:46:07 +0300

    Update problem28.java

--- EULER1-54/src/problem28/problem28.java
+++ EULER1-54/src/problem28/problem28.java
@@ -5,7 +5,7 @@ public class problem28 {
 	public static void main(String[] arg) {
 		int sum = 0;
 		int square = 1;
-		// generates odd primes;
+
 		for (int i = 1; i <= 1001 * 1001; i += square + 1) {
 			if (i % 2 == 1)
 				if ((int) Math.sqrt(i) * (int) Math.sqrt(i) == i)
